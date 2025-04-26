394f81c7691cdcfcd9dd8fbb823652900005013b
HolkarIntership <holkarintershipprogram@gmail.com>
2025-04-26 12:24:15 +0530

    about page me content change kiya

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -2,7 +2,14 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <div>
+
+      <h1>About</h1>
+
+      <h2>about page is created</h2>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequatur et voluptate ratione assumenda quia amet quae non, error aspernatur cum minus doloremque pariatur commodi obcaecati laudantium illo consequuntur ab quibusdam!</p>
+    </div>
   )
 }
 
